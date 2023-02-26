dbf77535ca7154d77528cb588396ed2cb2f8c7b7
Daniel Nagel <danielnagel@mailbox.org>
2023-02-26 15:53:21 +0100

    Removed unused const

--- src/main/js/src/components/Employee.vue
+++ src/main/js/src/components/Employee.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-const props = defineProps<{employee: Employee;}>();
+defineProps<{ employee: Employee; }>();
 </script>
 
 <template>
     <tr>
-        <td class="border border-zinc-600 py-1 px-3">{{employee.firstName}}</td>
-        <td class="border border-zinc-600 py-1 px-3">{{employee.lastName}}</td>
-        <td class="border border-zinc-600 py-1 px-3">{{employee.description}}</td>
+        <td class="border border-zinc-600 py-1 px-3">{{ employee.firstName }}</td>
+        <td class="border border-zinc-600 py-1 px-3">{{ employee.lastName }}</td>
+        <td class="border border-zinc-600 py-1 px-3">{{ employee.description }}</td>
     </tr>
 </template>
