13bc952867c71757a0020a985189732a01d11bb8
Daniel Nagel <danielnagel@mailbox.org>
2023-02-27 15:48:40 +0100

    Removed CORS implementation

--- src/main/java/com/reactandspring/demo/EmployeeRepository.java
+++ src/main/java/com/reactandspring/demo/EmployeeRepository.java
@@ -1,8 +1,6 @@
 package com.reactandspring.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
