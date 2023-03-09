c648b65508e751395ca1edd13f4f01fa8ace551e
Daniel Nagel <danielnagel@mailbox.org>
2023-03-09 09:06:24 +0100

    Added dev build with sourcemaps

--- src/main/js/vite.config.ts
+++ src/main/js/vite.config.ts
@@ -1,26 +1,38 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfigExport } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const config: UserConfigExport = {
   plugins: [vue()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/main.ts'),
-      name: "Bundle",
-      fileName: "bundle"
-    },
-    outDir: "../resources/static",
-    emptyOutDir: true
-  },
-  define: {
-      'process.env': {}
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8080'
+}
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  if (command === "serve") {
+    config.server = {
+      proxy: {
+        '/api': 'http://localhost:8080'
+      }
     }
+    config.build.sourcemap = "inline";
+    return config;
+  } else {
+    config.build = {
+      lib: {
+        entry: resolve(__dirname, 'src/main.ts'),
+        name: "Bundle",
+        fileName: "bundle"
+      },
+      outDir: "../resources/static",
+      emptyOutDir: true
+    };
+    config.define = {
+      'process.env': {}
+    };
+    if (mode === "development") {
+      config.build.sourcemap = "inline";
+    }
+    return config
   }
 })
